132ca37d5cf174cef77d1e5736d79546651bdf11
ManishMadan2882 <manishmadan321@gmail.com>
2023-07-26 19:57:50 +0530

    added c# lang

--- backend/controllers/compileCode.js
+++ backend/controllers/compileCode.js
@@ -1,5 +1,6 @@
 var tmp = require('tmp');
 var fs = require('fs');
+
 const {exec} = require('child_process')
 
 async function compile(req,res)
@@ -10,52 +11,40 @@ async function compile(req,res)
         java:".java",
         cpp:".cpp",
         c:".c",
+        cs:".cs",
         python:".py",
         javascript:".js"
        }
+
         tmp.file({prefix: 'project', postfix: extensions[language], keep: true},async  function (err, path, fd, cleanupCallback) {
   
             if (err) throw err;
                  
        console.log("File: ", path);
        console.log("Filedescriptor: ", fd);
-       await fs.writeFile(path, code,()=>console.log('written'))
+       fs.writeFile(path, code, () => console.log('written file'))
+      
        const commands = {
-        java:`javac ${path} && java Main`,
-        cpp:`gcc ${path}.c && \a.out`,
-        c:`gcc ${path}.c && \a.out`,
+        java:`javac ${path} && java ${path}`,
+        cpp:`g++ ${path} -o outputCPP && ./outputCPP`,
+        c:`gcc ${path} -o outputC && ./outputC`,
+        cs:`mcs ${path} && mono ${path.substring(0,path.length-3)}.exe`,
         python:`python ${path}`,
         javascript:`node ${path}`
        }
+       
        const child = exec(commands[language], { stdio: 'pipe' }, (error, stdout, stderr) => {
         if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
+         res.json({output : error.message});
+         return;
         }
-        console.log(`Output: ${stdout}`);
+        res.json({output : stdout});
       });
-      
       
       // Write the input to the child process
       child.stdin.write(input);
-      child.stdin.end(); // End the input stream
-      
-      // Handle output or errors if needed
-      
-      child.stdout.on('data', (data) => {
-        result = result  + data;
-        console.log(`Received output: ${data}`);
-        
-      });
-      
-      child.stderr.on('data', (data) => {
-        console.error(`Received error: ${data}`);
-      });
-      child.on('close', function(code) {
-        //Here you can get the exit code of the script
-        res.json({output : result});
-    })
-      
+      // End the input stream
+      child.stdin.end(); 
 });
 }
 
--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Editor } from '@monaco-editor/react';
+import ClipLoader from "react-spinners/ClipLoader";
 import { useState,useRef } from 'react';
 const CodeEditor = () => {
     const editorRef = useRef(null);
@@ -19,12 +20,15 @@ const CodeEditor = () => {
     const languages = [
         { value: "c", label: "C",default:"c" },
         { value: "cpp", label: "C++",default:"cpp" },
+        { value: "cs", label: "C#",default:"cs" },
         { value: "python", label: "Python",default:"py" },
         { value: "java", label: "Java",default:"java" },
         { value: "javascript", label: "Javascript",default:"js" }
     ];
     const textSizeOptions = [10,12,16,20,24,28,32,36,40,44];
     const compileCode = (code)=>{
+        setOutputLoading(true);
+        setOutput('');
         console.log({code:code,language:language,input:input});
         fetch('https://api-compile.onrender.com/api/v1/compile', {
             method: 'POST',
@@ -37,6 +41,7 @@ const CodeEditor = () => {
             .then(data =>{ 
                 console.log(data)
                 setOutput(data.output)
+                setOutputLoading(false);
             })
             .catch(error => console.error(error));
     }
@@ -45,7 +50,7 @@ const CodeEditor = () => {
     const  [output,setOutput] = useState('')
     const  [fontSize,setFontSize] = useState(16) 
     const  [language,setLanguage] = useState('java')
-   // const [code,setCode] = useState('')
+    const  [outputLoading, setOutputLoading] = useState(false);
     return (
         <div className='flex flex-wrap justify-normal bg-gray-800 h-full'>
           <div className='w-full  md:w-2/3'>
@@ -95,16 +100,26 @@ const CodeEditor = () => {
           </div>
           <div className='w-full  md:w-1/3 p-2'>
             <div className='w-full ' >
-                <label className="mb-2 text-sm font-medium inline dark:text-white text-gray-900">INPUT</label>
-                <button onClick={showValue} className=' bg-cyan-950 border-white border-[0.25px] hover:bg-gray-600 text-teal-200 rounded-md p-1 m-2'>
+                <label className="mb-2 text-sm font-medium inline text-white">INPUT</label>
+                <button onClick={showValue} className=' bg-cyan-950  border-white border-[0.25px] hover:bg-gray-600 text-teal-200 rounded-md p-1 m-2'>
                     RUN
                 </button>
-                <textarea value={input} rows="14" onChange={(e)=>setInput(e.target.value)} className=" resize-none block w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Testcase 0"/>
+                <textarea value={input} rows="14" onChange={(e)=>setInput(e.target.value)} className=" resize-none block w-full p-2.5 text-sm    bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Testcase 0"/>
             </div>
             <div className='w-full'>
-                <label className="block mb-2 text-sm font-medium dark:text-white text-gray-900">OUTPUT</label>
-                <pre className="p-2.5 w-full text-sm  rounded-lg border  bg-gray-700 border-gray-600 h-72 block placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Testcase 0">
+                <label className="block mb-2 text-sm font-medium text-white ">OUTPUT</label>
+                <pre className=" overflow-scroll editscroll p-2.5 w-full text-sm  rounded-lg border  bg-gray-700 border-gray-600 h-72 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Testcase 0">
                    {output}
+                  <div className='flex justify-center mt-6'>
+                  <ClipLoader
+                    color={"white"}
+                    loading={outputLoading}
+                    className=''
+                    size={100}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                    />
+                  </div>
                 </pre>
             </div>
           </div>
